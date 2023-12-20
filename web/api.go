4838e9a3052faba29f7c55bfe1e7c07320b58132
郑海成 <zhhcheng@yonyou.com>
2023-12-20 12:48:56 +0800

    绑定json参数

--- web/api.go
+++ web/api.go
@@ -1,6 +1,10 @@
 package web
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Ping(ctx *gin.Context) {
 
@@ -10,6 +14,22 @@ func Login(ctx *gin.Context) {
 
 }
 
+type registerReq struct {
+	Username string `json:"user_name" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"omitempty,email"`
+	Phone    string `json:"phone" binding:"required,e164"`
+}
+
 func Register(ctx *gin.Context) {
+	req := &registerReq{}
+	err := ctx.ShouldBindJSON(req)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, req)
 
 }
