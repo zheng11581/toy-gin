403a594f4356d7f9e033ca0a0f2ccf841974f1da
郑海成 <zhhcheng@yonyou.com>
2023-12-20 17:12:50 +0800

    user handler func

--- handlers/user/user.go
+++ handlers/user/user.go
@@ -1,11 +1,24 @@
-package course
+package user
 
 import (
+	"zheng11581/toy-gin/handlers"
+
 	"github.com/gin-gonic/gin"
 )
 
-func Get(ctx *gin.Context) {
+type userReq struct {
+	UserId   string
+	UserName string
+	Password string
+	Age      int
+	Sex      string
+}
 
+func Get(ctx *gin.Context) {
+	userId := ctx.Param("id")
+	user := &userReq{}
+	user.UserId = userId
+	handlers.WrapContext(ctx).Success(user)
 }
 
 func List(ctx *gin.Context) {
