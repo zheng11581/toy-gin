c56f36bc632ee8e84d2086054ad3c69030c79e9f
郑海成 <zhhcheng@yonyou.com>
2023-12-20 13:34:10 +0800

    绑定query和path参数

--- web/course.go
+++ web/course.go
@@ -1,9 +1,19 @@
 package web
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func GetCourseDetail(ctx *gin.Context) {
-
+	courseId := ctx.Param("id")
+	courseName := ctx.Query("name")
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "获取课程详情成功",
+		"id":      courseId,
+		"name":    courseName,
+	})
 }
 
 func GetCourseVideo(ctx *gin.Context) {
